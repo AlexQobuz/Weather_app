56b57961aef95d367c7711f8746bc3f72ed6624c
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-01-18 17:48:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/weather/Database.kt
@@ -0,0 +1,31 @@
+package com.example.weather
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+private const val DATABASE_NAME = "weather.db"
+private const val DATABASE_VERSION = 1
+
+private const val CITY_TABLE_NAME = "city"
+private const val CITY_KEY_ID = "id"
+private const val CITY_KEY_NAME = "name"
+
+private const val CITY_TABLE_CREATE = """
+    CREATE TABLE $CITY_TABLE_NAME (
+        $CITY_KEY_ID INTEGER PRIMARY KEY,
+        $CITY_KEY_NAME TEXT
+"""
+
+class Database(context: Context)
+    : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+}
