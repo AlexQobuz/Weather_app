ec1792831662e80166a61a281315674948097d0c
Alexandre <clemencot.alexandre@gmail.com>
2023-01-19 17:44:09 +0100

    construction du fragment city

--- app/src/main/java/com/example/weather/city/CityFragment.kt
+++ app/src/main/java/com/example/weather/city/CityFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weather.App
 import com.example.weather.App.Companion.database
 import com.example.weather.Database
 import com.example.weather.R
 
-class CityFragment : Fragment() {
+class CityFragment : Fragment(), CityAdapter.CityItemListener {
 
     private lateinit var cities: MutableList<City>
     private lateinit var database: Database
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +28,18 @@ class CityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_city, container, false)
+        recyclerView = view.findViewById(R.id.cities_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = CityAdapter(cities, this)
+        recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -41,6 +56,14 @@ class CityFragment : Fragment() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCitySelected(city: City) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCityDeleted(city: City) {
+        TODO("Not yet implemented")
     }
 
     private fun showCreateCityDialog() {
@@ -66,6 +89,5 @@ class CityFragment : Fragment() {
                 Toast.LENGTH_LONG).show()
         }
     }
-
 
 }
