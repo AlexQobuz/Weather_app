58e1ccb0cee56011f4557ff1e7f70c16c2a31112
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-01-19 10:29:35 +0100

    Création d'une ville dans la BDD

--- app/src/main/java/com/example/weather/Database.kt
+++ app/src/main/java/com/example/weather/Database.kt
@@ -3,6 +3,9 @@ package com.example.weather
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import androidx.core.content.contentValuesOf
+import com.example.weather.city.City
 
 private const val DATABASE_NAME = "weather.db"
 private const val DATABASE_VERSION = 1
@@ -21,6 +24,8 @@ private const val CITY_TABLE_CREATE = """
 class Database(context: Context)
     : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
+    val TAG = Database::class.java.simpleName
+
     // Ici je récupère la constante pour
     // créer la BDD et ensuite je la crée
     override fun onCreate(db: SQLiteDatabase?) {
@@ -29,6 +34,20 @@ class Database(context: Context)
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
 
+    }
+
+    // Ici cette fonction créer une ville dans la BDD
+    // et je lance un message quand je lance la fonction
+    fun createCity(city: City) : Boolean {
+        val values = contentValuesOf()
+        values.put(CITY_KEY_NAME, city.name)
+
+        Log.d(TAG, "Creating city: $values")
+
+        val id = writableDatabase.insert(CITY_TABLE_NAME, null, values)
+        city.id = id
+
+        return id > 0
     }
 
 }
